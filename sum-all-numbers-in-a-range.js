13c6e27a7142696fef69ed4885814e6f1f9c6be5
RomanBogatikov <40308050+RomanBogatikov@users.noreply.github.com>
2018-08-21 11:20:05 -0400

    Sum All Numbers In a Range algorithm solution

---
+++ sum-all-numbers-in-a-range.js
@@ -0,0 +1,12 @@
+function sumAll(arr) {
+    let sortedArr = arr.concat([]).sort(function(a,b) {
+      return a-b
+    });
+    let sum = 0;
+    for (let i = sortedArr[0]; i <= sortedArr[1]; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  sumAll([1, 4]);
